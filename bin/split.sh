d40fab89e4101bd09b06fbad8d7ea4c72b92b7c0
Ricardo Boss <contact@ricardoboss.de>
2021-11-02 11:52:41 +0100

    Fetch all commits

--- bin/split.sh
+++ bin/split.sh
@@ -11,7 +11,7 @@ CURRENT_BRANCH="main"
 
 function split()
 {
-    SHA1=$(./bin/splitsh-lite --prefix="$1" --origin=FETCH_HEAD)
+    SHA1=$(./bin/splitsh-lite --prefix="$1")
     git push "$2" "$SHA1:refs/heads/$CURRENT_BRANCH" -f
 }
 
