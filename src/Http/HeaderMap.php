ea6f40a9c1e376649162e0507cdcde356464587b
Ricardo Boss <contact@ricardoboss.de>
2021-11-04 12:26:31 +0100

    Added fromArray method

--- src/Http/HeaderMap.php
+++ src/Http/HeaderMap.php
@@ -2,6 +2,7 @@
 
 namespace Philly\Http;
 
+use InvalidArgumentException;
 use Philly\Collection\HashMap;
 
 /**
@@ -9,4 +10,34 @@ use Philly\Collection\HashMap;
  */
 class HeaderMap extends HashMap implements Contract\HeaderMap
 {
+	public static function fromArray(array $headers): self
+	{
+		$map = new self();
+
+		/** @var mixed $value */
+		foreach ($headers as $name => $value) {
+			if (is_string($value)) {
+				$value = [$value];
+			} else if (is_array($value)) {
+				$value = array_values(
+					array_map(
+						static fn($val) => is_string($val) ?
+							$val :
+							throw new InvalidArgumentException("Header value array can only contain string values"),
+						$value
+					)
+				);
+			} else {
+				throw new InvalidArgumentException("Header value must be an array or string, " . gettype($value) . " given");
+			}
+
+			if (is_string($name)) {
+				$map->put(HeaderName::fromString($name), $value);
+			} else {
+				throw new InvalidArgumentException("Header name must be a string");
+			}
+		}
+
+		return $map;
+	}
 }
