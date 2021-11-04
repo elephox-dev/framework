5cf5507f31d2d3ff99df7813d4581de42617747d
Ricardo Boss <contact@ricardoboss.de>
2021-11-04 12:25:35 +0100

    Removed Uri property and added Content property

--- src/Http/Contract/Response.php
+++ src/Http/Contract/Response.php
@@ -7,13 +7,13 @@ use Philly\Http\ResponseCode;
 
 interface Response
 {
-	public function setUri(string $uri): void;
-
-	public function getUri(): string;
-
 	public function getHeaders(): HeaderMap;
 
 	public function setCode(ResponseCode $code): void;
 
 	public function getCode(): ResponseCode;
+
+	public function setContent(string $content): void;
+
+	public function getContent(): string;
 }
