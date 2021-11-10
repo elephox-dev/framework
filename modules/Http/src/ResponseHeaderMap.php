6ba453740450bf1c1839a4407a377e7056f5b5f9
Ricardo Boss <contact@ricardoboss.de>
2021-11-10 17:16:45 +0100

    Fix unit tests

--- modules/Http/src/ResponseHeaderMap.php
+++ modules/Http/src/ResponseHeaderMap.php
@@ -19,16 +19,19 @@ class ResponseHeaderMap extends HeaderMap implements Contract\ResponseHeaderMap
 
 	public static function fromString(string $headers): self
 	{
-		/** @var ArrayList<KeyValuePair<string, string>> $headerRows */
-		$headerKeyValueList = Regex::split("\r?\n", $headers)
-			->map(static function (string $row): KeyValuePair {
-				if (!str_contains($row, ':')) {
-					throw new InvalidArgumentException("Invalid header row: $row");
-				}
+		$rows = Regex::split('/\n/', $headers);
+		$rows->pop(); // remove last empty row
 
-				[$name, $value] = explode(':', $row, 2);
-				return new KeyValuePair($name, trim($value));
-			});
+		/** @var ArrayList<KeyValuePair<string, string>> $headerRows */
+		$headerKeyValueList = $rows->map(static function (string $row): KeyValuePair {
+			if (!str_contains($row, ':')) {
+				throw new InvalidArgumentException("Invalid header row: $row");
+			}
+
+			[$name, $value] = explode(':', $row, 2);
+			return new KeyValuePair($name, trim($value));
+		});
+
 		/** @psalm-suppress InvalidArgument The generic types are subtypes of the expected ones. */
 		$headerMap = ArrayMap::fromKeyValuePairList($headerKeyValueList);
 
