7390e4bb06e3ae652994fb522bc74ee0414a4373
Ricardo Boss <contact@ricardoboss.de>
2021-11-10 16:24:56 +0100

    Added Regex helper class

---
+++ modules/Text/src/Regex.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Philly\Text;
+
+use InvalidArgumentException;
+use Philly\Collection\ArrayList;
+
+class Regex
+{
+	/**
+	 * @param string $pattern
+	 * @param string $subject
+	 * @param int $limit
+	 * @return ArrayList<string>
+	 */
+	public static function split(string $pattern, string $subject, int $limit = -1): ArrayList
+	{
+		$parts = preg_split($pattern, $subject, $limit);
+		if ($parts === false) {
+			throw new InvalidArgumentException('An error occurred while splitting: ' . preg_last_error_msg());
+		}
+
+		/** @var ArrayList<string> */
+		return ArrayList::fromArray($parts);
+	}
+}
