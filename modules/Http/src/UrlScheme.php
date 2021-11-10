3422752286d71bc7feae59fc571b702bd297f366
Ricardo Boss <contact@ricardoboss.de>
2021-11-10 20:12:42 +0100

    Removed unused method

--- modules/Http/src/UrlScheme.php
+++ modules/Http/src/UrlScheme.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Philly\Http;
 
-use InvalidArgumentException;
-
 enum UrlScheme: string {
 	case HTTPS = 'https';
 	case HTTP = 'http';
@@ -12,16 +10,4 @@ enum UrlScheme: string {
 	case FILE = 'file';
 	case MAILTO = 'mailto';
 	case SSH = 'ssh';
-
-	public function getDefaultPort(): int
-	{
-		return match ($this) {
-			self::HTTPS => 443,
-			self::HTTP => 80,
-			self::FTP => 21,
-			self::SSH => 22,
-			self::FILE,
-			self::MAILTO => throw new InvalidArgumentException("No default port for $this->value scheme"),
-		};
-	}
 }
