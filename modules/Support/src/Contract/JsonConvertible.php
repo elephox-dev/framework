e513e473986399fd05331f54324f8e900d75a550
Ricardo Boss <contact@ricardoboss.de>
2021-11-17 02:18:09 +0100

    Make toJson throw an exception

--- modules/Support/src/Contract/JsonConvertible.php
+++ modules/Support/src/Contract/JsonConvertible.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Elephox\Support\Contract;
 
+use JsonException;
+
 /**
  * Declares the implementation can be represented as a JSON string.
  */
@@ -11,10 +13,11 @@ interface JsonConvertible
 	/**
 	 * @param int $flags (optional) The same options you can pass to `json_encode`
 	 *
-	 * @return string|false Returns this object in its JSON representation or `false` on failure. This is the result of
-	 * passing this object to `json_encode` with the given options.
+	 * @return string Returns this object in its JSON representation. Throws a JsonException on failure. This is the
+	 * result of passing this object to `json_encode` with the given options.
 	 *
-	 * @see \json_encode()
+	 * @throws JsonException
+	 * @see json_encode()
 	 */
-	public function asJson(int $flags = 0): string|false;
+	public function toJson(int $flags = JSON_THROW_ON_ERROR): string;
 }
