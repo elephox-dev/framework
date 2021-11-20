381883e0dadddae6fa8ab1c8ebf4e3f57ac24b3f
Ricardo Boss <contact@ricardoboss.de>
2021-11-20 05:10:49 +0100

    Added helper attributes for HTTP request methods

---
+++ modules/Core/src/Handler/Attribute/Http/Head.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Core\Handler\Attribute\Http;
+
+use Attribute;
+use Elephox\Core\Handler\Attribute\RequestHandler;
+use Elephox\Core\Handler\UrlTemplate;
+use Elephox\Http\RequestMethod;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+class Head extends RequestHandler
+{
+	public function __construct(string|UrlTemplate $url)
+	{
+		parent::__construct($url, RequestMethod::HEAD);
+	}
+}
