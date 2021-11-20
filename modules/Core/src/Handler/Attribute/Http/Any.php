d6e2ec692197477b436d0b81b9dc844ffe309a30
Ricardo Boss <contact@ricardoboss.de>
2021-11-20 05:12:12 +0100

    Added helper for any request method

---
+++ modules/Core/src/Handler/Attribute/Http/Any.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Core\Handler\Attribute\Http;
+
+use Attribute;
+use Elephox\Core\Handler\Attribute\RequestHandler;
+use Elephox\Core\Handler\UrlTemplate;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+class Any extends RequestHandler
+{
+	public function __construct(string|UrlTemplate $url)
+	{
+		parent::__construct($url);
+	}
+}
