a21ba96d29e418d139951856b1850042cfe81710
Ricardo Boss <contact@ricardoboss.de>
2021-11-22 21:20:11 +0100

    Added Registrar trait

---
+++ modules/Core/src/Registrar.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Core;
+
+use Elephox\DI\Contract\Container;
+
+trait Registrar
+{
+	/** @var list<class-string> $classes */
+	public array $classes;
+
+	/** @var array<non-empty-string, object> $instances */
+	public array $instances;
+
+	/** @var array<non-empty-string, non-empty-string> $aliases */
+	public array $aliases;
+
+	public function registerClasses(Container $container): void
+	{
+		foreach ($this->classes as $class) {
+			$container->register($class);
+		}
+	}
+
+	public function registerInstances(Container $container): void
+	{
+		foreach ($this->instances as $contract => $instance) {
+			$container->register($contract, $instance);
+		}
+	}
+
+	public function registerAliases(Container $container): void
+	{
+		foreach ($this->aliases as $alias => $contract) {
+			$container->alias($alias, $contract);
+		}
+	}
+
+	public function registerAll(Container $container): void
+	{
+		$this->registerClasses($container);
+		$this->registerInstances($container);
+		$this->registerAliases($container);
+	}
+}
