5374ea221b26cd72d4bc06537c327b63a84995e7
Ricardo Boss <contact@ricardoboss.de>
2021-12-28 15:56:14 +0100

    Added test with orderBy

--- modules/PIE/test/ReadmeTest.php
+++ modules/PIE/test/ReadmeTest.php
@@ -12,16 +12,20 @@ class ReadmeTest extends TestCase
 {
 	public function testReadme(): void
 	{
-		$array = [1, 2, 3, 4, 5];
+		$array = [5, 2, 1, 4, 3];
 		$pie = PIE::from($array);
 
 		$sum = $pie->sum(fn (int $item) => $item);
 
 		self::assertEquals(15, $sum);
 
-		$sum2 = $pie->where(fn(int $item) => $item % 2 === 0)
+		$sum2 = $pie->where(fn (int $item) => $item % 2 === 0)
 					->sum(fn (int $item) => $item);
 
 		self::assertEquals(6, $sum2);
+
+		$ordered = $pie->orderBy(fn($a, $b) => $a <=> $b);
+
+		self::assertEquals([1, 2, 3, 4, 5], $ordered->toArray());
 	}
 }
