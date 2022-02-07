64b9cc474437855a8a36214619db006eea395acd
Ricardo Boss <contact@ricardoboss.de>
2022-02-07 18:00:58 +0100

    Removed references to safe

--- modules/OOR/src/Casing.php
+++ modules/OOR/src/Casing.php
@@ -19,35 +19,26 @@ class Casing
 		return mb_convert_case($string, MB_CASE_TITLE, 'UTF-8');
 	}
 
-	/**
-	 * @throws \Safe\Exceptions\PcreException
-	 */
 	public static function toCamel(string $string): string
 	{
 		/** @var string $result */
-		$result = \Safe\preg_replace('/([\s\-_]+)/', '', ucwords($string, '-_ '));
+		$result = preg_replace('/([\s\-_]+)/', '', ucwords($string, '-_ '));
 
 		return lcfirst($result);
 	}
 
-	/**
-	 * @throws \Safe\Exceptions\PcreException
-	 */
 	public static function toSnake(string $string): string
 	{
 		/** @var string $result */
-		$result = \Safe\preg_replace('/([A-Z])/', '_$1', self::toCamel($string));
+		$result = preg_replace('/([A-Z])/', '_$1', self::toCamel($string));
 
 		return self::toLower($result);
 	}
 
-	/**
-	 * @throws \Safe\Exceptions\PcreException
-	 */
 	public static function toKebab(string $string): string
 	{
 		/** @var string $result */
-		$result = \Safe\preg_replace('/([A-Z])/', '-$1', self::toCamel($string));
+		$result = preg_replace('/([A-Z])/', '-$1', self::toCamel($string));
 
 		return self::toLower($result);
 	}
