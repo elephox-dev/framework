a44762146793fa4f591f3316a51f273e3ed9703e
Ricardo Boss <contact@ricardoboss.de>
2022-04-05 23:14:44 +0200

    Fix tests

--- modules/Logging/test/MessageFormatterSinkTest.php
+++ modules/Logging/test/MessageFormatterSinkTest.php
@@ -33,11 +33,20 @@ class MessageFormatterSinkTest extends MockeryTestCase
 
 		$sink
 			->expects('write')
-			->with("Hello \033[0;32mWorld\033[0m", LogLevel::INFO, [])
+			->with("Hello \033[32mWorld\033[39m", LogLevel::INFO, [])
+			->andReturns()
+		;
+
+		$sink
+			->expects('write')
+			->with("This \033[32mis\033[33m a \033[31mwarning\033[33m", LogLevel::WARNING, [])
 			->andReturns()
 		;
 
 		$messageFormatterSink = new MessageFormatterSink($sink);
 		$messageFormatterSink->write("Hello <green>World</green>", LogLevel::INFO, []);
+		$messageFormatterSink->write("This <green>is</green> a <red>warning</red>", LogLevel::WARNING, []);
 	}
+
+	// TODO: write tests for background and options
 }
