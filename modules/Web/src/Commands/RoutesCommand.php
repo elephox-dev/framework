8b6e3da1b5c9bee0f237490b7ab03e2d465c2f73
Ricardo Boss <contact@ricardoboss.de>
2022-04-11 23:43:55 +0200

    Moved commands from inspector to web module

---
+++ modules/Web/src/Commands/RoutesCommand.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Web\Commands;
+
+use Elephox\Collection\Enumerable;
+use Elephox\Console\Command\CommandInvocation;
+use Elephox\Console\Command\CommandTemplateBuilder;
+use Elephox\Console\Command\Contract\CommandHandler;
+use Elephox\Logging\Contract\Logger;
+use Elephox\Web\Routing\Contract\RouteHandler;
+use Elephox\Web\Routing\Contract\Router;
+
+class RoutesCommand implements CommandHandler
+{
+	public function __construct(
+		private readonly Logger $logger,
+		private readonly Router $router,
+	) {
+	}
+
+	public function configure(CommandTemplateBuilder $builder): void
+	{
+		$builder
+			->name('routes')
+			->description('List all routes')
+		;
+	}
+
+	public function handle(CommandInvocation $command): int|null
+	{
+		$routes = Enumerable::from($this->router->getRouteHandlers());
+
+		$this->logger->info('Following routes where found:');
+		$this->logger->info('<cyan>weight</cyan> <blue>path regex</blue> => <yellow>handler</yellow>');
+
+		foreach ($routes->orderByDescending(fn (RouteHandler $r): int => $r->getSourceAttribute()->getWeight()) as $route) {
+			$attribute = $route->getSourceAttribute();
+
+			$this->logger->info(sprintf(
+				'<cyan>%s</cyan> <blue>%s</blue> => <yellow>%s</yellow>',
+				$attribute->getWeight(),
+				$route->getPathRegex(),
+				(string) $route,
+			));
+		}
+
+		return 0;
+	}
+}
---
+++ modules/Web/src/Commands/ServeCommand.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Web\Commands;
+
+use Elephox\Configuration\Contract\Environment;
+use Elephox\Console\Command\CommandInvocation;
+use Elephox\Console\Command\CommandTemplateBuilder;
+use Elephox\Console\Command\Contract\CommandHandler;
+use Elephox\Logging\Contract\Logger;
+use InvalidArgumentException;
+use RuntimeException;
+
+class ServeCommand implements CommandHandler
+{
+	public function __construct(
+		private readonly Logger $logger,
+		private readonly Environment $environment,
+	) {
+	}
+
+	public function configure(CommandTemplateBuilder $builder): void
+	{
+		$publicDir = $this->environment->getRoot()->getDirectory('public');
+
+		$builder
+			->name('serve')
+			->description('Starts the PHP built-in webserver for your application')
+			->argument('host', 'Host to bind to', 'localhost', false)
+			->argument('port', 'Port to bind to (>=1024, <=65535)', '8000', false)
+			->argument('root', 'Root directory to serve from', $publicDir->getPath(), false)
+			->argument('env', 'The environment to use (e.g. development, staging or production)', 'development', false)
+		;
+	}
+
+	public function handle(CommandInvocation $command): int|null
+	{
+		$host = $command->getArgument('host')->value;
+		$port = $command->getArgument('port')->value;
+		$root = $command->getArgument('root')->value;
+		$env = $command->getArgument('env')->value;
+
+		if (!ctype_digit($port)) {
+			throw new InvalidArgumentException('Port must be a number');
+		}
+
+		$port = (int) $port;
+		if ($port < 1 || $port > 65535) {
+			throw new InvalidArgumentException('Port must be between 1 and 65535');
+		}
+
+		if (!is_dir($root)) {
+			throw new InvalidArgumentException('Root directory (' . $root . ') does not exist');
+		}
+
+		$root = realpath($root);
+
+		$this->logger->info('Starting PHP built-in webserver on ' . $host . ':' . $port);
+
+		$process = proc_open(
+			sprintf('%s -S %s:%d', escapeshellarg(PHP_BINARY), $host, $port),
+			[STDIN],
+			$pipes,
+			$root,
+			array_merge(getenv(), $_ENV, ['APP_ENV' => $env]),
+		);
+
+		if ($process === false) {
+			throw new RuntimeException('Failed to start server');
+		}
+
+		$status = proc_get_status($process);
+		if ($status['running'] === false) {
+			$this->logger->error('Server exited unexpectedly');
+
+			return -1;
+		}
+
+		register_shutdown_function(static fn (): bool => proc_terminate($process));
+
+		$this->logger->info('Server process started', ['pid' => $status['pid'], 'env' => $env, 'command' => $status['command']]);
+
+		while ($status = proc_get_status($process)) {
+			if (!$status['running']) {
+				break;
+			}
+
+			sleep(1);
+		}
+
+		$this->logger->warning(sprintf('Server process exited with %d', $status['exitcode']));
+
+		return 0;
+	}
+}
