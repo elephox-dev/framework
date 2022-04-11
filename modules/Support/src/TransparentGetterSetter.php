0fe80e3112f40b7f78a4adccf371fcd044fc7398
Ricardo Boss <contact@ricardoboss.de>
2022-04-11 16:03:44 +0200

    Added test for more method names

--- modules/Support/src/TransparentGetterSetter.php
+++ modules/Support/src/TransparentGetterSetter.php
@@ -8,40 +8,64 @@ use Elephox\OOR\Casing;
 
 trait TransparentGetterSetter
 {
-	protected function buildGetterName(string $propertyName): string
+	/**
+	 * @return iterable<mixed, string>
+	 */
+	protected function buildGetterNames(string $propertyName): iterable
 	{
-		return 'get' . Casing::toPascal($propertyName);
+		$getterPropertyName = Casing::toPascal($propertyName);
+		yield 'get' . $getterPropertyName;
+		yield 'is' . $getterPropertyName;
+		yield 'has' . $getterPropertyName;
 	}
 
-	protected function buildSetterName(string $propertyName): string
+	/**
+	 * @return iterable<mixed, string>
+	 */
+	protected function buildSetterNames(string $propertyName): iterable
 	{
-		return 'set' . Casing::toPascal($propertyName);
+		$setterPropertyName = Casing::toPascal($propertyName);
+
+		yield 'set' . $setterPropertyName;
+		yield 'put' . $setterPropertyName;
 	}
 
 	public function __get(string $name)
 	{
-		$method = $this->buildGetterName($name);
-		if (method_exists($this, $method)) {
-			return $this->$method();
+		$tried = [];
+		foreach ($this->buildGetterNames($name) as $method) {
+			if (method_exists($this, $method)) {
+				return $this->$method();
+			}
+
+			$tried[] = $method;
 		}
 
-		throw new BadMethodCallException('Method ' . $method . ' does not exist.');
+		throw new BadMethodCallException('None of the tried getter methods exists: ' . implode(', ', $tried));
 	}
 
 	public function __set(string $name, $value)
 	{
-		$method = $this->buildSetterName($name);
-		if (method_exists($this, $method)) {
-			return $this->$method($value);
+		$tried = [];
+		foreach ($this->buildSetterNames($name) as $method) {
+			if (method_exists($this, $method)) {
+				return $this->$method($value);
+			}
+
+			$tried[] = $method;
 		}
 
-		throw new BadMethodCallException('Method ' . $method . ' does not exist.');
+		throw new BadMethodCallException('None of the tried setter methods exists: ' . implode(', ', $tried));
 	}
 
 	public function __isset(string $name)
 	{
-		$method = $this->buildGetterName($name);
+		foreach ($this->buildGetterNames($name) as $method) {
+			if (method_exists($this, $method)) {
+				return true;
+			}
+		}
 
-		return method_exists($this, $method);
+		return false;
 	}
 }
