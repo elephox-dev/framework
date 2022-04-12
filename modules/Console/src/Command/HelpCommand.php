34c63e34246afb1b87b2b1a8ecfa2079e7fd8306
Ricardo Boss <contact@ricardoboss.de>
2022-04-12 12:49:14 +0200

    Improved help command formatting with new Console::table helper

--- modules/Console/src/Command/HelpCommand.php
+++ modules/Console/src/Command/HelpCommand.php
@@ -6,6 +6,7 @@ namespace Elephox\Console\Command;
 use Elephox\Collection\Iterator\FlipIterator;
 use Elephox\Logging\Contract\Logger;
 use ricardoboss\Console;
+use Stringable;
 
 class HelpCommand implements Contract\CommandHandler
 {
@@ -30,11 +31,23 @@ class HelpCommand implements Contract\CommandHandler
 		if (!is_string($requestedCommand)) {
 			$this->logger->info(Console::underscore('Available commands:'));
 
+			/** @var array<int, array<int, string>> $commands */
+			$commands = [
+				[Console::yellow('Command'), 'Description'],
+			];
+
 			/**
 			 * @var CommandTemplate $commandTemplate
 			 */
 			foreach (new FlipIterator($this->commands->getIterator()) as $commandTemplate) {
-				$this->logger->info(sprintf("\t%s\t\t%s", Console::yellow($commandTemplate->name), empty($commandTemplate->description) ? Console::gray('No description') : $commandTemplate->description));
+				$description = empty($commandTemplate->description) ? Console::gray('No description') : $commandTemplate->description;
+				$name = Console::yellow($commandTemplate->name);
+
+				$commands[] = [$name, $description];
+			}
+
+			foreach (Console::table($commands, compact: true) as $line) {
+				$this->logger->info($line);
 			}
 
 			return 0;
@@ -43,10 +56,44 @@ class HelpCommand implements Contract\CommandHandler
 		try {
 			$commandTemplate = $this->commands->getTemplateByName($requestedCommand);
 
-			$this->logger->info(Console::underscore(sprintf("Help for command '%s':", Console::yellow($commandTemplate->name))));
-			$this->logger->info(sprintf("\t%s\t\t\t%s", Console::green('Name'), $commandTemplate->name));
-			$this->logger->info(sprintf("\t%s\t\t%s", Console::green('Description'), empty($commandTemplate->description) ? Console::gray('No description') : $commandTemplate->description));
-			$this->logger->info(sprintf("\t%s\t\t%s", Console::green('Arguments'), empty($commandTemplate->argumentTemplates) ? Console::gray('No arguments') : implode(' ', $commandTemplate->argumentTemplates->select(static fn (ArgumentTemplate $t) => ($t->required ? '<' : '[') . $t->name . ($t->required ? '>' : ']'))->toList())));
+			$this->logger->info(Console::underscore(sprintf("Help for command '%s':", $commandTemplate->name)));
+
+			$metaData = [
+				[Console::green('Name'), "\t", $commandTemplate->name],
+				[Console::green('Description'), "\t", (empty($commandTemplate->description) ? Console::gray('No description') : $commandTemplate->description)],
+			];
+
+			foreach (Console::table($metaData, noBorder: true) as $line) {
+				$this->logger->info($line);
+			}
+
+			$this->logger->info(Console::green('Arguments:'));
+
+			$argumentData = [];
+			foreach ($commandTemplate->argumentTemplates as $argumentTemplate) {
+				$openBracket = $argumentTemplate->required ? '<' : '[';
+				$closeBracket = $argumentTemplate->required ? '>' : ']';
+				$name = $openBracket . $argumentTemplate->name;
+				if (!$argumentTemplate->required) {
+					$name .= '=' . match (get_debug_type($argumentTemplate->default)) {
+						'null' => 'null',
+						'bool' => $argumentTemplate->default ? 'true' : 'false',
+						'array' => implode(',', $argumentTemplate->default),
+						'int', 'float', 'string', Stringable::class => (string) $argumentTemplate->default,
+						default => get_debug_type($argumentTemplate->default),
+					};
+				}
+				$name .= $closeBracket;
+				$name = $argumentTemplate->required ? Console::blue($name) : Console::light_gray($name);
+
+				$description = empty($argumentTemplate->description) ? Console::gray('No description') : $argumentTemplate->description;
+
+				$argumentData[] = ["\t", $name, "\t", $description];
+			}
+
+			foreach (Console::table($argumentData, noBorder: true) as $line) {
+				$this->logger->info($line);
+			}
 
 			return 0;
 		} catch (CommandNotFoundException) {
