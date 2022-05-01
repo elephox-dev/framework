1ce60dd15952ecc24fb903935e8c0c4fdbe5f5b0
Ricardo Boss <contact@ricardoboss.de>
2022-05-01 20:26:37 +0200

    Rewritten casing test to test each input with every alternative

--- modules/OOR/test/CasingTest.php
+++ modules/OOR/test/CasingTest.php
@@ -12,141 +12,66 @@ use PHPUnit\Framework\TestCase;
  */
 class CasingTest extends TestCase
 {
-	public function testToLower(): void
+	public function casingDataProvider(): iterable
 	{
-		static::assertEquals('foo', Casing::toLower('Foo'));
-		static::assertEquals('foo', Casing::toLower('foo'));
-		static::assertEquals('foobar', Casing::toLower('FooBar'));
-		static::assertEquals('foo bar', Casing::toLower('foO Bar'));
-		static::assertEquals('foo-bar', Casing::toLower('foO-Bar'));
-		static::assertEquals('foo_bar', Casing::toLower('FOO_BAR'));
+		yield ['foo', 'foo', 'FOO', 'Foo', 'foo', 'foo', 'foo', 'Foo', 'FOO', 'Foo'];
+		yield ['FOO', 'foo', 'FOO', 'Foo', 'foo', 'foo', 'foo', 'Foo', 'FOO', 'Foo'];
+		yield ['FooBar', 'foobar', 'FOOBAR', 'Foobar', 'foobar', 'foobar', 'foobar', 'Foobar', 'FOOBAR', 'Foobar'];
+		yield ['Foo Bar', 'foo bar', 'FOO BAR', 'Foo Bar', 'fooBar', 'foo_bar', 'foo-bar', 'Foo-Bar', 'FOO-BAR', 'FooBar'];
+		yield ['FoO BaR', 'foo bar', 'FOO BAR', 'Foo Bar', 'fooBar', 'foo_bar', 'foo-bar', 'Foo-Bar', 'FOO-BAR', 'FooBar'];
+		yield ['Foo-Bar', 'foo-bar', 'FOO-BAR', 'Foo-Bar', 'fooBar', 'foo_bar', 'foo-bar', 'Foo-Bar', 'FOO-BAR', 'FooBar'];
+		yield ['Foo_Bar', 'foo_bar', 'FOO_BAR', 'Foo_Bar', 'fooBar', 'foo_bar', 'foo-bar', 'Foo-Bar', 'FOO-BAR', 'FooBar'];
+		yield ['f%a@d_=', 'f%a@d_=', 'F%A@D_=', 'F%A@D_=', 'f%A@D=', 'f%a@d_=', 'f%a@d-=', 'F%A@D-=', 'F%A@D-=', 'F%A@D='];
 	}
 
-	public function testToUpper(): void
+	/**
+	 * @dataProvider casingDataProvider
+	 *
+	 * @param string $input
+	 * @param string $lower
+	 * @param string $upper
+	 * @param string $title
+	 * @param string $camel
+	 * @param string $snake
+	 * @param string $kebab
+	 * @param string $httpHeader
+	 * @param string $cobol
+	 * @param string $pascal
+	 */
+	public function testCasing(string $input, string $lower, string $upper, string $title, string $camel, string $snake, string $kebab, string $httpHeader, string $cobol, string $pascal): void
 	{
-		static::assertEquals('FOO', Casing::toUpper('Foo'));
-		static::assertEquals('FOO', Casing::toUpper('foo'));
-		static::assertEquals('FOOBAR', Casing::toUpper('FooBar'));
-		static::assertEquals('FOO BAR', Casing::toUpper('foO Bar'));
-		static::assertEquals('FOO-BAR', Casing::toUpper('foO-Bar'));
-		static::assertEquals('FOO_BAR', Casing::toUpper('foo_bar'));
+		static::assertSame($lower, Casing::toLower($input));
+		static::assertSame($upper, Casing::toUpper($input));
+		static::assertSame($title, Casing::toTitle($input));
+		static::assertSame($camel, Casing::toCamel($input));
+		static::assertSame($snake, Casing::toSnake($input));
+		static::assertSame($kebab, Casing::toKebab($input));
+		static::assertSame($httpHeader, Casing::toHttpHeader($input));
+		static::assertSame($cobol, Casing::toCobol($input));
+		static::assertSame($pascal, Casing::toPascal($input));
 	}
 
-	public function testToTitle(): void
+	public function replaceDelimitersDataProvider(): iterable
 	{
-		static::assertEquals('Foo', Casing::toTitle('foo'));
-		static::assertEquals('Foo', Casing::toTitle('Foo'));
-		static::assertEquals('Foobar', Casing::toTitle('FooBar'));
-		static::assertEquals('Foo Bar', Casing::toTitle('foo bar'));
-		static::assertEquals('Foo Bar', Casing::toTitle('Foo Bar'));
-		static::assertEquals('Foo Bar', Casing::toTitle('FOO BAR'));
-		static::assertEquals('Foo-Bar', Casing::toTitle('foo-bar'));
-		static::assertEquals('Foo-Bar', Casing::toTitle('Foo-Bar'));
-		static::assertEquals('Foo-Bar', Casing::toTitle('FOO-BAR'));
-		static::assertEquals('Foo_Bar', Casing::toTitle('foo_bar'));
-		static::assertEquals('Foo_Bar', Casing::toTitle('Foo_Bar'));
-		static::assertEquals('Foo_Bar', Casing::toTitle('FOO_BAR'));
+		yield ['foo\nbar', 'foo bar', '\n', null];
+		yield ['example.com', 'example.com', ' ', null];
+		yield ['example com', 'example.com', ' ', '/\\./'];
 	}
 
-	public function testReplaceDelimiters(): void
+	/**
+	 * @dataProvider replaceDelimitersDataProvider
+	 *
+	 * @param string $output
+	 * @param string $input
+	 * @param string $replacement
+	 * @param ?string $delimitersPattern
+	 */
+	public function testReplaceDelimiters(string $output, string $input, string $replacement, ?string $delimitersPattern): void
 	{
-		static::assertEquals('foo\nbar', Casing::replaceDelimiters('foo bar', '\n'));
-		static::assertEquals('example.com', Casing::replaceDelimiters('example.com', ' '));
-	}
-
-	public function testToCamel(): void
-	{
-		static::assertEquals('foo', Casing::toCamel('foo'));
-		static::assertEquals('foo', Casing::toCamel('Foo'));
-		static::assertEquals('foobar', Casing::toCamel('FooBar'));
-		static::assertEquals('fooBar', Casing::toCamel('foo bar'));
-		static::assertEquals('fooBar', Casing::toCamel('Foo Bar'));
-		static::assertEquals('fooBar', Casing::toCamel('FOO BAR'));
-		static::assertEquals('fooBar', Casing::toCamel('foo-bar'));
-		static::assertEquals('fooBar', Casing::toCamel('Foo-Bar'));
-		static::assertEquals('fooBar', Casing::toCamel('FOO-BAR'));
-		static::assertEquals('fooBar', Casing::toCamel('foo_bar'));
-		static::assertEquals('fooBar', Casing::toCamel('Foo_Bar'));
-		static::assertEquals('fooBar', Casing::toCamel('FOO_BAR'));
-	}
-
-	public function testToSnake(): void
-	{
-		static::assertEquals('foo', Casing::toSnake('foo'));
-		static::assertEquals('foo', Casing::toSnake('Foo'));
-		static::assertEquals('foobar', Casing::toSnake('FooBar'));
-		static::assertEquals('foo_bar', Casing::toSnake('foo bar'));
-		static::assertEquals('foo_bar', Casing::toSnake('Foo Bar'));
-		static::assertEquals('foo_bar', Casing::toSnake('FOO BAR'));
-		static::assertEquals('foo_bar', Casing::toSnake('foo-bar'));
-		static::assertEquals('foo_bar', Casing::toSnake('Foo-Bar'));
-		static::assertEquals('foo_bar', Casing::toSnake('FOO-BAR'));
-		static::assertEquals('foo_bar', Casing::toSnake('foo_bar'));
-		static::assertEquals('foo_bar', Casing::toSnake('Foo_Bar'));
-		static::assertEquals('foo_bar', Casing::toSnake('FOO_BAR'));
-	}
-
-	public function testToKebab(): void
-	{
-		static::assertEquals('foo', Casing::toKebab('foo'));
-		static::assertEquals('foo', Casing::toKebab('Foo'));
-		static::assertEquals('foobar', Casing::toKebab('FooBar'));
-		static::assertEquals('foo-bar', Casing::toKebab('foo bar'));
-		static::assertEquals('foo-bar', Casing::toKebab('Foo Bar'));
-		static::assertEquals('foo-bar', Casing::toKebab('FOO BAR'));
-		static::assertEquals('foo-bar', Casing::toKebab('foo-bar'));
-		static::assertEquals('foo-bar', Casing::toKebab('Foo-Bar'));
-		static::assertEquals('foo-bar', Casing::toKebab('FOO-BAR'));
-		static::assertEquals('foo-bar', Casing::toKebab('foo_bar'));
-		static::assertEquals('foo-bar', Casing::toKebab('Foo_Bar'));
-		static::assertEquals('foo-bar', Casing::toKebab('FOO_BAR'));
-	}
-
-	public function testToHttpHeader(): void
-	{
-		static::assertEquals('Foo', Casing::toHttpHeader('foo'));
-		static::assertEquals('Foo', Casing::toHttpHeader('Foo'));
-		static::assertEquals('Foobar', Casing::toHttpHeader('FooBar'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('foo bar'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('Foo Bar'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('FOO BAR'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('foo-bar'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('Foo-Bar'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('FOO-BAR'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('foo_bar'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('Foo_Bar'));
-		static::assertEquals('Foo-Bar', Casing::toHttpHeader('FOO_BAR'));
-	}
-
-	public function testToCobol(): void
-	{
-		static::assertEquals('FOO', Casing::toCobol('foo'));
-		static::assertEquals('FOO', Casing::toCobol('Foo'));
-		static::assertEquals('FOOBAR', Casing::toCobol('FooBar'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('foo bar'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('Foo Bar'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('FOO BAR'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('foo-bar'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('Foo-Bar'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('FOO-BAR'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('foo_bar'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('Foo_Bar'));
-		static::assertEquals('FOO-BAR', Casing::toCobol('FOO_BAR'));
-	}
-
-	public function testToPascal(): void
-	{
-		static::assertEquals('Foo', Casing::toPascal('foo'));
-		static::assertEquals('Foo', Casing::toPascal('Foo'));
-		static::assertEquals('Foobar', Casing::toPascal('FooBar'));
-		static::assertEquals('FooBar', Casing::toPascal('foo bar'));
-		static::assertEquals('FooBar', Casing::toPascal('Foo Bar'));
-		static::assertEquals('FooBar', Casing::toPascal('FOO BAR'));
-		static::assertEquals('FooBar', Casing::toPascal('foo-bar'));
-		static::assertEquals('FooBar', Casing::toPascal('Foo-Bar'));
-		static::assertEquals('FooBar', Casing::toPascal('FOO-BAR'));
-		static::assertEquals('FooBar', Casing::toPascal('foo_bar'));
-		static::assertEquals('FooBar', Casing::toPascal('Foo_Bar'));
-		static::assertEquals('FooBar', Casing::toPascal('FOO_BAR'));
+		if ($delimitersPattern !== null) {
+			static::assertEquals($output, Casing::replaceDelimiters($input, $replacement, $delimitersPattern));
+		} else {
+			static::assertEquals($output, Casing::replaceDelimiters($input, $replacement));
+		}
 	}
 }
