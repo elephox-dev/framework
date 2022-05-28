88859909d994edb9fbf28db1ef38d69a87ac0339
Ricardo Boss <contact@ricardoboss.de>
2022-05-28 14:04:59 +0200

    Updated test

--- modules/Logging/test/SimpleFormatColorSinkTest.php
+++ modules/Logging/test/SimpleFormatColorSinkTest.php
@@ -20,6 +20,12 @@ class SimpleFormatColorSinkTest extends MockeryTestCase
 		$sink = M::mock(Sink::class);
 
 		$sink
+			->expects('hasCapability')
+			->with(SinkCapability::AnsiFormatting)
+			->andReturns(true)
+		;
+
+		$sink
 			->expects('write')
 			->with(LogLevel::INFO, 'Hello World', [])
 			->andReturns()
@@ -34,20 +40,59 @@ class SimpleFormatColorSinkTest extends MockeryTestCase
 		$sink = M::mock(Sink::class);
 
 		$sink
+			->allows('hasCapability')
+			->twice()
+			->with(SinkCapability::AnsiFormatting)
+			->andReturns(true)
+		;
+
+		$sink
 			->expects('write')
-			->with(LogLevel::INFO, "Hello \033[32mWorld\033[39m", [])
+			->with(M::capture($firstLogLevel), "Hello \033[32mWorld\033[39m", [])
 			->andReturns()
 		;
 
 		$sink
 			->expects('write')
-			->with(LogLevel::WARNING, "This \033[32mis\033[33m a \033[31mwarning\033[33m", [])
+			->with(M::capture($secondLogLevel), "This \033[32mis\033[39m a \033[31mwarning\033[39m", [])
 			->andReturns()
 		;
 
 		$messageFormatterSink = new SimpleFormatColorSink($sink);
 		$messageFormatterSink->write(LogLevel::INFO, 'Hello <green>World</green>', []);
+		static::assertEquals(LogLevel::INFO, $firstLogLevel);
 		$messageFormatterSink->write(LogLevel::WARNING, 'This <green>is</green> a <red>warning</red>', []);
+		static::assertEquals(LogLevel::WARNING, $secondLogLevel);
+	}
+
+	public function testFormatRemovedWithNoAnsiSupport(): void
+	{
+		$sink = M::mock(Sink::class);
+
+		$sink
+			->allows('hasCapability')
+			->twice()
+			->with(SinkCapability::AnsiFormatting)
+			->andReturns(false)
+		;
+
+		$sink
+			->expects('write')
+			->with(M::capture($firstLogLevel), "Hello World", [])
+			->andReturns()
+		;
+
+		$sink
+			->expects('write')
+			->with(M::capture($secondLogLevel), "This is a warning", [])
+			->andReturns()
+		;
+
+		$messageFormatterSink = new SimpleFormatColorSink($sink);
+		$messageFormatterSink->write(LogLevel::INFO, 'Hello <green>World</green>', []);
+		static::assertEquals(LogLevel::INFO, $firstLogLevel);
+		$messageFormatterSink->write(LogLevel::WARNING, 'This <green>is</green> a <red>warning</red>', []);
+		static::assertEquals(LogLevel::WARNING, $secondLogLevel);
 	}
 
 	// TODO: write tests for background and options
