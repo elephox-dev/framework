79534d335bdf9b710ace6cb46f3e4a0471aa6e48
Ricardo Boss <contact@ricardoboss.de>
2022-06-13 23:14:30 +0200

    Also log warnings to STDERR

--- modules/Logging/src/StandardSink.php
+++ modules/Logging/src/StandardSink.php
@@ -10,7 +10,7 @@ class StandardSink implements Sink
 {
 	public function write(LogLevelContract $level, string $message, array $context): void
 	{
-		if ($level->getLevel() > LogLevel::WARNING->getLevel()) {
+		if ($level->getLevel() >= LogLevel::WARNING->getLevel()) {
 			fwrite(STDERR, $message . PHP_EOL);
 		} else {
 			fwrite(STDOUT, $message . PHP_EOL);
