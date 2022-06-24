622b43836d8e25e84ae52ca077545de191f83ea1
Ricardo Boss <mail@ricardoboss.de>
2022-06-24 12:46:35 +0200

    Replaced remaining references to path

--- modules/Files/src/File.php
+++ modules/Files/src/File.php
@@ -97,18 +97,18 @@ class File extends AbstractFilesystemNode implements Contract\File
 	#[Pure]
 	public function getExtension(): string
 	{
-		return pathinfo($this->path, PATHINFO_EXTENSION);
+		return pathinfo($this->getPath(), PATHINFO_EXTENSION);
 	}
 
 	public function getSize(): int
 	{
 		if (!$this->exists()) {
-			throw new FileNotFoundException($this->path);
+			throw new FileNotFoundException($this->getPath());
 		}
 
-		$size = filesize($this->path);
+		$size = filesize($this->getPath());
 		if ($size === false) {
-			throw new RuntimeException("Unable to get the size of file ($this->path)");
+			throw new RuntimeException("Unable to get the size of file ({$this->getPath()})");
 		}
 
 		return $size;
@@ -123,10 +123,10 @@ class File extends AbstractFilesystemNode implements Contract\File
 	public function getHash(): string
 	{
 		if (!$this->exists()) {
-			throw new FileNotFoundException($this->path);
+			throw new FileNotFoundException($this->getPath());
 		}
 
-		$hash = md5_file($this->path);
+		$hash = md5_file($this->getPath());
 		if ($hash === false) {
 			throw new RuntimeException('Could not hash file');
 		}
@@ -138,40 +138,40 @@ class File extends AbstractFilesystemNode implements Contract\File
 	public function isReadable(): bool
 	{
 		/** @psalm-suppress ImpureFunctionCall */
-		return is_readable($this->path);
+		return is_readable($this->getPath());
 	}
 
 	public function isWritable(): bool
 	{
 		if (!$this->exists()) {
-			throw new FileNotFoundException($this->path);
+			throw new FileNotFoundException($this->getPath());
 		}
 
-		return is_writable($this->path);
+		return is_writable($this->getPath());
 	}
 
 	#[Pure]
 	public function isExecutable(): bool
 	{
-		return is_executable($this->path);
+		return is_executable($this->getPath());
 	}
 
 	public function exists(): bool
 	{
-		return file_exists($this->path);
+		return file_exists($this->getPath());
 	}
 
 	public function copyTo(FilesystemNode $node, bool $overwrite = true): Contract\File
 	{
 		if (!$this->exists()) {
-			throw new FileNotFoundException($this->path);
+			throw new FileNotFoundException($this->getPath());
 		}
 
 		$destination = $this->getDestination($node, $overwrite);
 
-		$success = copy($this->path, $destination->getPath());
+		$success = copy($this->getPath(), $destination->getPath());
 		if (!$success) {
-			throw new FileCopyException($this->path, $destination->getPath());
+			throw new FileCopyException($this->getPath(), $destination->getPath());
 		}
 
 		return new self($destination->getPath());
@@ -180,30 +180,30 @@ class File extends AbstractFilesystemNode implements Contract\File
 	public function delete(): void
 	{
 		if (!$this->exists()) {
-			throw new FileNotFoundException($this->path);
+			throw new FileNotFoundException($this->getPath());
 		}
 
-		if (!unlink($this->path)) {
-			throw new FileDeleteException($this->path);
+		if (!unlink($this->getPath())) {
+			throw new FileDeleteException($this->getPath());
 		}
 	}
 
 	public function moveTo(FilesystemNode $node, bool $overwrite = true): Contract\File
 	{
 		if (!$this->exists()) {
-			throw new FileNotFoundException($this->path);
+			throw new FileNotFoundException($this->getPath());
 		}
 
 		$destination = $this->getDestination($node, $overwrite);
 
-		if (is_uploaded_file($this->path)) {
-			$success = move_uploaded_file($this->path, $destination->getPath());
+		if (is_uploaded_file($this->getPath())) {
+			$success = move_uploaded_file($this->getPath(), $destination->getPath());
 		} else {
-			$success = rename($this->path, $destination->getPath());
+			$success = rename($this->getPath(), $destination->getPath());
 		}
 
 		if (!$success) {
-			throw new FileMoveException($this->path, $destination->getPath());
+			throw new FileMoveException($this->getPath(), $destination->getPath());
 		}
 
 		return new self($destination->getPath());
