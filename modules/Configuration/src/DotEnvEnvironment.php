6264ed0c44d4147ebad7874ce061e67cac333e05
Ricardo Boss <contact@ricardoboss.de>
2022-11-03 23:12:10 +0100

    Widen return type to file contract

--- modules/Configuration/src/DotEnvEnvironment.php
+++ modules/Configuration/src/DotEnvEnvironment.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 namespace Elephox\Configuration;
 
+use Elephox\Files\Contract\File as FileContract;
 use Elephox\Files\File;
 
 abstract class DotEnvEnvironment extends AbstractEnvironment
 {
-	public function getDotEnvFileName(bool $local = false, ?string $envName = null): File
+	public function getDotEnvFileName(bool $local = false, ?string $envName = null): FileContract
 	{
 		$envFile = '.env';
 		if ($envName !== null) {
