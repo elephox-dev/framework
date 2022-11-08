141d40dbd751fbffa4ff2cb607200d69393e357c
Ricardo Boss <contact@ricardoboss.de>
2022-11-08 16:27:19 +0100

    Updated packages

--- modules/Http/src/UploadedFileMap.php
+++ modules/Http/src/UploadedFileMap.php
@@ -22,9 +22,6 @@ class UploadedFileMap extends ArrayMap implements Contract\UploadedFileMap
 
 		$map = new self();
 
-		/**
-		 * @var array{name: string, type: string, size: int, error: int, tmp_name: string, full_path: string} $file
-		 */
 		foreach ($files as $id => $file) {
 			$clientFilename = $file['name'];
 			$clientType = $file['type'];
