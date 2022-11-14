41a23d12ea63662b23861ac0514912f947b22336
Ricardo Boss <contact@ricardoboss.de>
2022-11-14 15:25:56 +0100

    Check if stdout and stderr are defined

--- modules/Logging/src/StandardSink.php
+++ modules/Logging/src/StandardSink.php
@@ -5,9 +5,17 @@ namespace Elephox\Logging;
 
 use Elephox\Logging\Contract\LogLevel as LogLevelContract;
 use Elephox\Logging\Contract\Sink;
+use const STDERR;
+use const STDOUT;
 
 class StandardSink implements Sink
 {
+	public function __construct()
+	{
+		assert(defined("STDOUT"), sprintf("Cannot use %s because STDOUT is not defined", __CLASS__));
+		assert(defined("STDERR"), sprintf("Cannot use %s because STDERR is not defined", __CLASS__));
+	}
+
 	public function write(LogLevelContract $level, string $message, array $context): void
 	{
 		if ($level->getLevel() >= LogLevel::WARNING->getLevel()) {
