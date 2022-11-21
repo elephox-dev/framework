4577eb8e53c04a0553e44e03952ad7646992a894
Ricardo Boss <contact@ricardoboss.de>
2022-11-21 23:30:10 +0100

    Marked clock methods as pure

--- modules/Clock/src/AbstractClock.php
+++ modules/Clock/src/AbstractClock.php
@@ -6,12 +6,15 @@ namespace Elephox\Clock;
 use DateTimeImmutable;
 use Elephox\Clock\Contract\Clock;
 use Elephox\Clock\Contract\Duration as DurationContract;
+use JetBrains\PhpStorm\Pure;
 use StellaMaris\Clock\ClockInterface;
 
 abstract class AbstractClock implements Clock
 {
+	#[Pure]
 	public function diff(ClockInterface $clock): DurationContract
 	{
+		/** @psalm-suppress ImpureMethodCall */
 		$diff = $this->now()->diff($clock->now());
 
 		return Duration::from(
@@ -26,23 +29,28 @@ abstract class AbstractClock implements Clock
 		);
 	}
 
+	#[Pure]
 	public function equals(ClockInterface $clock): bool
 	{
+		/** @psalm-suppress ImpureMethodCall */
 		return $this->now()->getTimestamp() === $clock->now()->getTimestamp();
 	}
 
+	#[Pure]
 	public function compare(ClockInterface $clock): int
 	{
 		$diff = $this->diff($clock);
 
-		return Duration::from()->compare($diff);
+		return Duration::zero()->compare($diff);
 	}
 
+	#[Pure]
 	public function add(DurationContract $duration): Clock
 	{
 		return new LazyClock(fn (): DateTimeImmutable => $this->now()->add($duration->toDateInterval()));
 	}
 
+	#[Pure]
 	public function sub(DurationContract $duration): Clock
 	{
 		return new LazyClock(fn (): DateTimeImmutable => $this->now()->sub($duration->toDateInterval()));
--- modules/Clock/src/Contract/Clock.php
+++ modules/Clock/src/Contract/Clock.php
@@ -3,17 +3,23 @@ declare(strict_types=1);
 
 namespace Elephox\Clock\Contract;
 
+use JetBrains\PhpStorm\Pure;
 use StellaMaris\Clock\ClockInterface;
 
 interface Clock extends ClockInterface
 {
+	#[Pure]
 	public function diff(ClockInterface $clock): Duration;
 
+	#[Pure]
 	public function add(Duration $duration): self;
 
+	#[Pure]
 	public function sub(Duration $duration): self;
 
+	#[Pure]
 	public function equals(ClockInterface $clock): bool;
 
+	#[Pure]
 	public function compare(ClockInterface $clock): int;
 }
