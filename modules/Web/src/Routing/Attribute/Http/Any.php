24fd5e0d1654772f2c0fd5e7d3405754e303ad2a
Ricardo Boss <contact@ricardoboss.de>
2022-11-23 16:31:10 +0100

    Add all request methods instead of empty

--- modules/Web/src/Routing/Attribute/Http/Any.php
+++ modules/Web/src/Routing/Attribute/Http/Any.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Elephox\Web\Routing\Attribute\Http;
 
 use Attribute;
+use Elephox\Http\RequestMethod;
 use Elephox\Web\Routing\Attribute\Contract\RouteAttribute;
 use Elephox\Web\Routing\Attribute\Controller;
 
@@ -12,6 +13,6 @@ class Any extends Controller implements RouteAttribute
 {
 	public function __construct(?string $path = self::DEFAULT_PATH, int $weight = self::DEFAULT_WEIGHT)
 	{
-		parent::__construct($path, $weight);
+		parent::__construct($path, $weight, RequestMethod::cases());
 	}
 }
