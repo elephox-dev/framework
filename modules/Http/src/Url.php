elephox-dev/framework

commit f5729f5928374e0550d2fc8bd9f39ecaac5859c6

Fixed psalm errors

modules/Http/src/Url.php
@@ -29,7 +29,7 @@ class Url implements Stringable, UriInterface
 			$matches,
 		);
 		/**
-		 * @var array{scheme: string, user: string, pass: string, host: string, port: string, path: string, query: string, fragment: string} $matches
+		 * @var array{scheme: string, user: string, pass: string, host?: string, host2?: string, port?: string, port2?: string, path: string, query: string, fragment: string} $matches
 		 */
 		if ($matches['scheme'] !== '') {
 			$scheme = $matches['scheme'];
@@ -45,17 +45,17 @@ class Url implements Stringable, UriInterface
 		$password = $matches['pass'] === '' ? null : $matches['pass'];
 		$builder->userInfo($username, $password);
 
-		if ($matches['host'] !== '') {
+		if (isset($matches['host'])) {
 			$builder->host($matches['host']);
-		} elseif ($matches['host2'] !== '') {
+		} elseif (isset($matches['host2'])) {
 			$builder->host($matches['host2']);
 		} else {
 			$builder->host(null);
 		}
 
-		if ($matches['port'] !== '' && ctype_digit($matches['port'])) {
+		if (isset($matches['port']) && ctype_digit($matches['port'])) {
 			$builder->port((int) $matches['port']);
-		} elseif ($matches['port2'] !== '' && ctype_digit($matches['port2'])) {
+		} elseif (isset($matches['port2']) && ctype_digit($matches['port2'])) {
 			$builder->port((int) $matches['port2']);
 		} else {
 			$builder->port(null);
@@ -244,6 +244,7 @@ class Url implements Stringable, UriInterface
 
 	public function withScheme($scheme): static
 	{
+		/** @psalm-suppress DocblockTypeContradiction */
 		if (!is_string($scheme)) {
 			throw new InvalidArgumentException("Expected type 'string', got " . get_debug_type($scheme));
 		}
@@ -260,10 +261,12 @@ class Url implements Stringable, UriInterface
 
 	public function withUserInfo($user, $password = null): static
 	{
+		/** @psalm-suppress DocblockTypeContradiction */
 		if (!is_string($user)) {
 			throw new InvalidArgumentException("Expected type 'string', got " . get_debug_type($user));
 		}
 
+		/** @psalm-suppress DocblockTypeContradiction */
 		if (!is_string($password) && $password !== null) {
 			throw new InvalidArgumentException("Expected type 'string' or 'null', got " . get_debug_type($password));
 		}
@@ -278,6 +281,7 @@ class Url implements Stringable, UriInterface
 
 	public function withHost($host): static
 	{
+		/** @psalm-suppress DocblockTypeContradiction */
 		if (!is_string($host)) {
 			throw new InvalidArgumentException("Expected type 'string', got " . get_debug_type($host));
 		}
@@ -292,6 +296,7 @@ class Url implements Stringable, UriInterface
 
 	public function withPort($port): static
 	{
+		/** @psalm-suppress DocblockTypeContradiction */
 		if (!is_int($port) && $port !== null) {
 			throw new InvalidArgumentException("Expected type 'int' or 'null', got " . get_debug_type($port));
 		}
@@ -306,6 +311,7 @@ class Url implements Stringable, UriInterface
 
 	public function withPath($path): static
 	{
+		/** @psalm-suppress DocblockTypeContradiction */
 		if (!is_string($path)) {
 			throw new InvalidArgumentException("Expected type 'string', got " . get_debug_type($path));
 		}
@@ -320,6 +326,7 @@ class Url implements Stringable, UriInterface
 
 	public function withQuery($query): static
 	{
+		/** @psalm-suppress DocblockTypeContradiction */
 		if (!is_string($query)) {
 			throw new InvalidArgumentException("Expected type 'string', got " . get_debug_type($query));
 		}
@@ -336,6 +343,7 @@ class Url implements Stringable, UriInterface
 
 	public function withFragment($fragment): static
 	{
+		/** @psalm-suppress DocblockTypeContradiction */
 		if (!is_string($fragment)) {
 			throw new InvalidArgumentException("Expected type 'string', got " . get_debug_type($fragment));
 		}

modules/Cache/src/TempDirCache.php
@@ -58,6 +58,7 @@ class TempDirCache extends AbstractCache
 	public function getItem(string $key): CacheItemInterface
 	{
 		if ($this->hasItem($key)) {
+			/** @var CacheItemInterface */
 			return $this->floatingItems->get($key);
 		}
 

modules/Clock/src/AbstractClock.php
@@ -44,13 +44,13 @@ abstract class AbstractClock implements Clock
 	}
 
 	#[Pure]
-	public function add(DurationContract $duration): Clock
+	public function add(DurationContract $duration): LazyClock
 	{
 		return new LazyClock(fn (): DateTimeImmutable => $this->now()->add($duration->toDateInterval()));
 	}
 
 	#[Pure]
-	public function sub(DurationContract $duration): Clock
+	public function sub(DurationContract $duration): LazyClock
 	{
 		return new LazyClock(fn (): DateTimeImmutable => $this->now()->sub($duration->toDateInterval()));
 	}

modules/Clock/src/AbstractDuration.php
@@ -130,7 +130,7 @@ abstract class AbstractDuration implements DurationContract, Stringable
 	{
 		$end = $start->add($this);
 		$periodSeconds = $this->getTotalSeconds() / $divisions;
-		$period = Duration::from($periodSeconds < 0, seconds: abs($periodSeconds));
+		$period = Duration::from($periodSeconds < 0, seconds: (int)abs($periodSeconds));
 
 		return new PeriodIterator($start, $period, $end);
 	}

modules/Clock/src/Duration.php
@@ -62,20 +62,21 @@ class Duration extends AbstractDuration
 		);
 	}
 
+	#[Pure]
 	public static function fromTotalMicroseconds(float $total): Contract\Duration
 	{
 		$negative = $total < 0;
 
-		$total = abs($total);
+		$totalAbs = abs($total);
 
-		$secondsPart = (int) ($total / self::MICROSECONDS_PER_SECOND);
+		$secondsPart = (int) ($totalAbs / self::MICROSECONDS_PER_SECOND);
 		$minutesPart = (int) ($secondsPart / self::SECONDS_PER_MINUTE);
 		$hoursPart = (int) ($minutesPart / self::MINUTES_PER_HOUR);
 		$daysPart = (int) ($hoursPart / self::HOURS_PER_DAY);
 		$monthsPart = (int) ($daysPart / self::DAYS_PER_MONTH);
 		$years = (int) ($monthsPart / self::MONTHS_PER_YEAR);
 
-		$microseconds = fmod($total, self::MICROSECONDS_PER_SECOND);
+		$microseconds = fmod($totalAbs, self::MICROSECONDS_PER_SECOND);
 		$seconds = (int) fmod($secondsPart, self::SECONDS_PER_MINUTE);
 		$minutes = (int) fmod($minutesPart, self::MINUTES_PER_HOUR);
 		$hours = (int) fmod($hoursPart, self::HOURS_PER_DAY);

modules/Collection/src/ArrayList.php
@@ -175,6 +175,11 @@ class ArrayList implements GenericList
 		return $this->items[$index];
 	}
 
+	/**
+	 * @return T
+	 *
+	 * @param int $index
+	 */
 	public function removeAt(int $index): mixed
 	{
 		if ($index < 0 || $index >= $this->count()) {

modules/Collection/src/ArrayMap.php
@@ -85,6 +85,13 @@ class ArrayMap implements GenericMap, ArrayAccess
 		return !$existed;
 	}
 
+	/**
+	 * @param TKey $key
+	 *
+	 * @return TValue
+	 *
+	 * @throws OffsetNotFoundException
+	 */
 	public function get(mixed $key): mixed
 	{
 		$validKey = $this->validateKey($key);

modules/Collection/src/Iterator/GroupingIterator.php
@@ -56,7 +56,6 @@ class GroupingIterator implements Iterator
 		$groupKey = $this->groupKeys[$idx];
 		$pairs = $this->groupPairs[$idx];
 
-		/** @psalm-suppress UnusedClosureParam */
 		$iterator = new SelectIterator(
 			new KeySelectIterator(
 				new ArrayIterator($pairs),

commit 346c340e499991ecc7a56342afc89d892f30de00

Fixed regression from f5729f5928374e0550d2fc8bd9f39ecaac5859c6

modules/Http/src/Url.php
@@ -45,9 +45,9 @@ class Url implements Stringable, UriInterface
 		$password = $matches['pass'] === '' ? null : $matches['pass'];
 		$builder->userInfo($username, $password);
 
-		if (isset($matches['host'])) {
+		if (isset($matches['host']) && $matches['host'] !== '') {
 			$builder->host($matches['host']);
-		} elseif (isset($matches['host2'])) {
+		} elseif (isset($matches['host2']) && $matches['host2'] !== '') {
 			$builder->host($matches['host2']);
 		} else {
 			$builder->host(null);