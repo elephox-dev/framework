c1fd266d192c4b83204889479160f711f6abfa39
Ricardo Boss <contact@ricardoboss.de>
2023-04-03 02:01:12 +0200

    Add pure attribute

--- modules/Files/src/LinkLoopDetectedException.php
+++ modules/Files/src/LinkLoopDetectedException.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace Elephox\Files;
 
+use JetBrains\PhpStorm\Pure;
 use Throwable;
 
 class LinkLoopDetectedException extends FileException
 {
+	#[Pure]
 	public function __construct(Contract\Link $a, Contract\Link $b, int $code = 0, ?Throwable $previous = null)
 	{
 		parent::__construct(sprintf('Symlink loop detected between (%s) and (%s)', $a->path(), $b->path()), $code, $previous);
