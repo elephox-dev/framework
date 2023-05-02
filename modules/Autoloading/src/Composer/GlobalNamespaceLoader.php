c929655e53a2462839cea2d65db56fdb97d56a4b
Ricardo Boss <contact@ricardoboss.de>
2023-05-02 11:01:12 +0200

    Removed unused imports

--- modules/Autoloading/src/Composer/GlobalNamespaceLoader.php
+++ modules/Autoloading/src/Composer/GlobalNamespaceLoader.php
@@ -3,16 +3,6 @@ declare(strict_types=1);
 
 namespace Elephox\Autoloading\Composer;
 
-use Composer\Autoload\ClassLoader;
-use Elephox\Collection\ArrayList;
-use Elephox\Collection\ArrayMap;
-use Elephox\Collection\Contract\GenericKeyedEnumerable;
-use Elephox\Files\Contract\Directory as DirectoryContract;
-use Elephox\Files\Directory;
-use Elephox\OOR\Regex;
-use Elephox\Autoloading\Composer\Contract\ComposerAutoloaderInit;
-use RuntimeException;
-
 final class GlobalNamespaceLoader
 {
 	/**
